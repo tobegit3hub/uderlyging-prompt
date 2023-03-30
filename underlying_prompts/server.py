d06d7eb063904d0cbeab0a10ff814c58f92eafc1
tobe <tobeg3oogle@gmail.com>
2023-03-30 16:46:55 +0800

    Do not support stream mode yet

--- underlying_prompts/server.py
+++ underlying_prompts/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, Response
 import requests
 import sys
 import logging
@@ -31,14 +31,38 @@ def handle_request(path):
         logger.info(f"OpenAI Request: {json_data}")
 
         url = f"https://api.openai.com/{path}"
-        response = requests.post(
-            url,
-            headers=headers,
-            json=json_data
-        )
-        logger.info(f"OpenAI Response: {response.json()}")
 
-        return response.json()
+        if "stream" in json_data and json_data["stream"] == True:
+            logger.error("Stream mode is not supported yet")
+            return "Stream mode is not supported yet"
+            """
+            response = requests.post(
+                url,
+                headers=headers,
+                json=json_data, 
+                stream=True
+            )
+            def generate():
+                for chunk in response.iter_content(chunk_size=1024):
+                    response = requests.post(
+                        url,
+                        headers=headers,
+                        json=json_data, 
+                        data=chunk
+                    )
+                    yield chunk
+            return_response =  Response(generate(), content_type='application/octet-stream')
+            """
+        else:
+            response = requests.post(
+                url,
+                headers=headers,
+                json=json_data
+            )
+            return_response =  response.json()
+
+        logger.info(f"OpenAI Response: {response.json()}")
+        return return_response
 
 
 def main():    
@@ -46,7 +70,8 @@ def main():
         port = int(sys.argv[1])
     else:
         port = 8888
-    app.run(port=port)
+    debug = False
+    app.run(host="0.0.0.0", port=port, debug=debug)
 
 
 if __name__ == '__main__':
