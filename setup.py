e065e03ff406e63ac3fba0c055636171a3942ed7
tobe <tobeg3oogle@gmail.com>
2023-03-30 16:24:02 +0800

    Add readme in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='underlying-prompts',
     version='0.1.1',
     author="tobe",
     author_email="tobeg3oogle@gmail.com",
     url="https://github.com/tobegit3hub/underlying-prompts",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'requests',
